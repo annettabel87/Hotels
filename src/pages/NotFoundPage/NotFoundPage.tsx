d26adb6610507722f20c4493244e48d0693e355f
unknown <annettabel@rambler.ru>
2024-03-03 21:01:03 +0500

    add notFoundPage

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,3 +1,20 @@
+import { useNavigate } from 'react-router-dom';
+import { ROUTE } from '../../constants/constants';
+import { Button, Result } from 'antd';
+import styles from './NotFoundPage.module.css';
+
 export const NotFoundPage = () => {
-  return <div>NotFoundPage</div>;
+  const navigate = useNavigate();
+  return (
+    <div className={styles.container}>
+      <Result
+        title="Такой страницы не существует."
+        extra={
+          <Button type="primary" onClick={() => navigate(ROUTE.MAIN)}>
+            на главную
+          </Button>
+        }
+      />
+    </div>
+  );
 };
